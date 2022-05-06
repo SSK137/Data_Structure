1851220e48919fc508de8c23d81ca9b7f7daebef
SSK137 <saurabhkodam@gmail.com>
2022-05-06 09:50:10 +0530

    Data Structure : Simple Create Link List

--- src/Linked_List.java
+++ src/Linked_List.java
@@ -1,5 +1,16 @@
 public class Linked_List {
     public static void main(String[] args) {
         System.out.println("Welcome To Linked List");
+
+        Node node = new Node(56);
+        Node node1 = new Node(30);
+        Node node2 = new Node(70);
+        node.next = node1;
+        node1.next = node2;
+        Node temp = node;
+        while (temp != null){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
     }
 }
